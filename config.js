71629f978ee76ee81b7b01a7a1503cab91dfab21
Karlita85 <Karlita90jose@gmail.com>
2024-05-21 13:02:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import { en, es, id, ar, pt } from './lib/idiomas/total-idiomas.js'
 
 //---------[ Añada los numeros a ser Propietario/a ]---------
 
-global.owner = [['573147616444', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], ['593968585383'], ['5492266466080'], ['5492266613038'], ['5491155983299'], ['595986172767'], ['5492964650915'], ['51948705559']]
+global.owner = [['', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], []]
 global.mods = [] 
 global.prems = []
 
@@ -87,14 +87,14 @@ global.md = 'https://github.com/elrebelde21/LoliBot-MD'
 global.fb = 'https://www.facebook.com/elrebelde21'
 global.face = 'https://www.facebook.com/groups/872989990425789/'
 
-global.nna = 'https://whatsapp.com/channel/0029Va4QjH7DeON0ePwzjS1A' //Update
-global.nn = 'https://chat.whatsapp.com/HNDVUxHphPzG3cJHIwCaX5' //Grupo 1
-global.nnn = 'https://chat.whatsapp.com/H4hxytyGvucIF1k0UAR7es' //Grupo 2
-global.nnnt = 'https://chat.whatsapp.com/IlyJBy1fxcw2qAHLSC5YLa' //Grupo del Colaboracion
-global.nnnt2 = 'https://chat.whatsapp.com/K7RzcVdxGqhGP5dCpVENSI' // Grupo COL 2
-global.nnntt = 'https://chat.whatsapp.com/IB9Vs7mZ03BBkH3reCU8Dw' //Grupo COL 3
-global.nnnttt = 'https://chat.whatsapp.com/KlqNmoUcVnsGJxIfATIDrK' //enlace lolibot
-global.nnntttt = 'https://chat.whatsapp.com/FRkr7jJHSJA5OjVtE64dDs' //Grupo ayuda sobre el bot
+global.nna = 'https://whatsapp.com/channel/0029Va4QjH7DeO0ePwzjS' //Update
+global.nn = 'https://chat.whatsapp.com/HNDVUxHphPzG3cJHIwCa' //Grupo 1
+global.nnn = 'https://chat.whatsapp.com/H4hxytyGvucIF1k0UARs' //Grupo 2
+global.nnnt = 'https://chat.whatsapp.com/IlyJBy1fxcw2qAHLSCLa' //Grupo del Colaboracion
+global.nnnt2 = 'https://chat.whatsapp.com/K7RzcVdxGqhGP5dCpVSI' // Grupo COL 2
+global.nnntt = 'https://chat.whatsapp.com/IB9Vs7mZ03BBkH3reCDw' //Grupo COL 3
+global.nnnttt = 'https://chat.whatsapp.com/KlqNmoUcVnsGJxIfATIK' //enlace lolibot
+global.nnntttt = 'https://chat.whatsapp.com/FRkr7jJHSJA5OjVtE6Ds' //Grupo ayuda sobre el bot
 global.bot = 'Wa.me/573147616444'
 global.asistencia = `${fb}`
 global.redes = [nna, yt, nn, md, tiktok, fb, nnn, face]
